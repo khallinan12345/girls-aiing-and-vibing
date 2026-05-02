f494b1cc267a9952b9476c65b873064242f354ce
khallinan12345 <kevin.hallinan.ud@gmail.com>
2026-05-01 21:50:54 -0400

    fix: move popups inside JSX return, add missing closing tags

--- src/pages/tech-skills/AIWorkflowDevPage.tsx
+++ src/pages/tech-skills/AIWorkflowDevPage.tsx
@@ -1849,8 +1849,6 @@ const AIWorkflowDevPage: React.FC = () => {
             </div>
           </div>
         </div>
-      </main>
-    </div>
 
         {/* Teaching Popup */}
         {showTeachingPopup && taskInstruction?.subTaskTeaching?.[subTaskIndex] && (
